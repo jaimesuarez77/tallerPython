4cf07644a10836e5c85d4ad908c682b1e898b3c0
camilo restrepo <69006663+CamiloRestrepo469@users.noreply.github.com>
2021-03-13 21:24:37 -0500

    s

---
+++ ejercicionatacion.py
@@ -0,0 +1,36 @@
+print("bienvenido a la escula de natacion ")
+solicitud=input("deseo ingresar a uno de nuestros cursos  si o  no    ")
+
+nino=0
+adolecentes=0
+contadorninos=0
+contadoradolecentes=0
+
+basico=120000
+experimentado=250000
+profesional=280000
+
+while (solicitud=="si"):
+    edad=int(input("cual es su  edad debe de ser entre 4 anos o 12 anos   : "))
+    if(edad<=4)or(edad<=8):
+        contadorninos+=1
+    elif (edad>=9)or(edad<=12):
+        contadoradolecentes+=1
+    elif (edad>13)or(edad>=10000):
+        print("ya estas mayor para este curso por favor saca la cc y pongase a trabajar")
+    curso=input("cual es el curso que desea basico,experimentado,profesional  :  ")
+    if (curso=="basico"):
+        basico=120000
+        if (curso=="experimentado"):
+            experimentado=250000
+            if (curso=="profesional"):
+                profesional=280000
+    elif  (curso=="  ")or(curso!="basico")or(curso!="experimentado")or(curso!="profesional"):
+        print("este curso no esta disponible o no existe ")
+    solicitud=input("deseo ingresar a uno de nuestros cursos  si o  no    ")
+
+controltotaldeninos=contadorninos+contadoradolecentes
+
+
+print("total de ninos son  : ",controltotaldeninos)
+print("el curso que deseo cuesta  : ",curso)
