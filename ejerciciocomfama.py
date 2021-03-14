62b551707368e584a17df941b67c488b86b9073c
camilo restrepo <69006663+CamiloRestrepo469@users.noreply.github.com>
2021-03-13 20:13:16 -0500

    taller Python

---
+++ ejerciciocomfama.py
@@ -0,0 +1,19 @@
+solicitud=input("desea ingresar (si o no)")
+adultos=0
+ninos=0
+
+contadorninos=0
+contadoradultos=0
+
+while (solicitud=="si"):
+    edad=int(input("cuantos anos tienes?  "))
+    if(edad<=15):
+        contadorninos+=1
+    else:
+        contadoradultos+=1
+    solicitud=input("desea ingresar (si o no)")
+
+contadortotalpersonas=contadorninos+contadoradultos
+
+print("el porcentaje adulto es  ",round(contadoradultos/contadortotalpersonas,2))
+print("el porcentaje nino es  ",round(contadorninos/contadortotalpersonas,2))
---
+++ ejerciciowhile.py
@@ -0,0 +1,22 @@
+
+
+contador=0
+contador2=0
+centinela=input("ingrese si o no ")
+while (centinela=="si"): 
+    genero=input("ingrese su genero")
+    if (genero=="mujer"):
+        contador=contador+1
+    else:
+        contador2=contador2+1
+    
+    
+    print("ingrese si desea continuar  ")
+    continuar=input("si o no")
+
+
+contador_suma=contador+contador2
+
+print("el numero de los hombres es   ",contador)
+print("el numero de los mujeres es   ",contador2)
+print("el numero de hombres y mujeres es    ",contador_suma)
