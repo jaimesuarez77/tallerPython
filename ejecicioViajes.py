98624cf9692eda67cf5eab86f3e7c822850ef39c
CamiloRestrepo469 <rsgermancamilo@gmail.com>
2021-03-20 19:21:33 -0500

    ejercicios

---
+++ ejecicioViajes.py
@@ -0,0 +1,69 @@
+capurgana=1500000
+nuqui=1200000
+islamargarita=200
+islagalapagos=400
+mukura=1200000
+islasanandres=1500000
+# tasa
+tasacapurgana=0.15
+tasanuqui=0.09
+tasamargarita=.016
+tasagalapagos=0.08
+tasamukura=0.09
+tasasanandres=0.12
+# impuestos
+impuestoscapurgana=0.2
+impuestosnuqui=0.19
+impuestosmargarita=0.11
+impuestosgalapagos=0.09
+impuestosmukura=0.11
+impuestossanandres=0.13
+pasajeros=0
+tarifatotal=0
+contador=0
+viajes=0
+
+breakcentinela=input('desea salir de vaciones con covid-19 incluido si o no?  ')
+while (breakcentinela=='si'):
+    pasajeros=float(input('numero de pasajeros que desean viajar   '))  
+    contador=0
+    while (contador<pasajeros):
+
+        print('eliga un numero para su destino ')
+        print('"1" = capurgana ')
+        print('"2" = nuqui  ')
+        print('"3" = isla margarita  ')
+        print('"4" = isla galapago  ')
+        print('"5" = isla mukura  ')
+        print('"6" = isla san andres  ')
+
+        viajes=0
+        viajes=float(input('eliga un numero para su destino\n'))  
+
+        if (pasajeros==1):
+            contador+=1
+            pasajeros+=1
+        elif (pasajeros==2):
+            contador+1
+            pasajeros+=1
+        elif (pasajeros==3):
+            contador+1
+            pasajeros+=1
+        elif (pasajeros==4):
+            contador+1
+            pasajeros+=1
+        elif (pasajeros==5):
+            contador+1
+            pasajeros+=1
+        elif (pasajeros==6):
+            contador+1
+            pasajeros+=1
+        tarifatotal=contador+pasajeros+viajes
+print('valor de la tarifa   ',tarifatotal,'\n')
+print('total de pasajeros  ',pasajeros,'\n')
+
+
+       
+    
+
+
--- ejerciEscuela.py
+++ ejerciEscuela.py
@@ -1,9 +1,19 @@
-gradoCursos=0
-edad=0
-cont=0
+contador =0 
+nombreEquipo=''
+nombrejugador=''
+documento=''
+posicionJugador=''
 
-contadorgradoCursos=0
-contadorEdad=0
-
-while (cont<2):
-    cursos=float(input("Ingrese el"))
+while (contador<3):
+    nombreEquipo=input('ingrese el nombre del equipo   ')
+    contador+=1
+    contador1=0
+    while (contador1<3):
+        contador1+=1
+        nombrejugador=input('ingrese el nombre del jugador  ')
+        documento=input('ingrese el documento del jugador  ')
+        posicionJugador=input('en que posicion juega  ')
+        print('nombre de los equipos a participar  : '+ nombreEquipo)
+        print('nombre de los jugadores del equipo  : '+ nombrejugador)
+        print('posicion del jugador en la cancha   : '+ posicionJugador)
+print('estamos listos para jugar')
