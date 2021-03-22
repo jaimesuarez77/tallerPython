6e3f6b7dae6b26e384a0246a0ebe1cfb73ba74f8
jaimesuarez77 <jaimesuarez77@gmail.com>
2021-03-21 22:05:52 -0500

    se hacen algunos cambios

--- ejercicioparapentes.py
+++ ejercicioparapentes.py
@@ -13,24 +13,40 @@ porcontajehombres=0
 participantes=0
 contadorgenero=0
 
-consentimiento=input('este es el consentimiento para poder volar en el parapente')
+consentimiento=input('Este es el consentimiento para poder volar en el parapente esta de acuerdo?: ')
 while (consentimiento=='si'):
-    participantes=float(input('nemero de participantes para volar como gallinas'))
+    participantes=float(input('número de participantes para volar como gallinas: '))
     contador=0
+    promedioM = 0
+    promedioF = 0
     while (contador<participantes):
         edad=float(input('cuantos anos tiene  : '))
-        genero=float(input('genero m = masculino f = femenino '))
-        eps=input('que eps esta afiliado')
+        contador+=1
+    #    genero=float(input('genero m = masculino f = femenino'))
+        eps=input('que eps esta afiliado: ')
         if (edad>15 and edad<=60):
-            contador+=1
+            
             contadoredad+=1
-        elif  (genero=="m"):
+        else:
+            print("No tienes la edad adecuada para volar en parapente...")
+            consentimiento = 'no' 
+        genero=input('genero m = masculino f = femenino: ')       
+        if  (genero=="m"):
             contadorgeneromasculino+=1
+            print(contador)
+            print(contadorgeneromasculino)
         elif  (genero=="f"):
             contadorgenerofemino+=1
-        print('promedio de mayores  ', contadoredad)
-        print('promedio de hombres', contadorgeneromasculino)
-        print('promedio de de mujeres  ', contadorgenerofemino)
-
+            print(contador)
+            print(contadorgenerofemino)
+    promedioM = (contadorgeneromasculino / contador) * 100 
+    promedioF = (contadorgenerofemino / contador) * 100
+    recauro = 80000*contador    
+    print('promedio de mayores  ', contadoredad)
+    print('El total de hombres es: ', contadorgeneromasculino)
+    print('promedio de hombres', round(promedioM),'%' )
+    print('promedio de de mujeres  ', round(promedioF),'%')
+    print('El total recaudado es de: ', recaudado)    
+    consentimiento=input('Este es el consentimiento para poder volar en el parapente esta de acuerdo? :') 
 
 
